6a113bf395fb4bcb9ad2fa24bc5616b1d25a088b
t-liu <thomas.s.liu@gmail.com>
2022-03-25 23:55:11 -0400

    add ignore files; remove task definition data file; major updates to main terraform file

--- main.tf
+++ main.tf
@@ -26,80 +26,83 @@ provider "aws" {
 
 # vpc
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/24"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
-    tags       = {
-        Name = "Terraform VPC"
-    }
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Terraform VPC"
+  }
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_subnet" "pub_subnet" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "10.1.0.0/22"
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/26"
+  tags = {
+    Name = "Terraform Public Subnet"
+  }
 }
 
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.internet_gateway.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
 }
 
 resource "aws_route_table_association" "route_table_association" {
-    subnet_id      = aws_subnet.pub_subnet.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.public.id
 }
 
 # security groups
 resource "aws_security_group" "ecs_sg" {
-    vpc_id      = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    ingress {
-        from_port       = 22
-        to_port         = 22
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port       = 443
-        to_port         = 443
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port       = 0
-        to_port         = 65535
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "rds_sg" {
-    vpc_id      = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    ingress {
-        protocol        = "tcp"
-        from_port       = 3306
-        to_port         = 3306
-        cidr_blocks     = ["0.0.0.0/0"]
-        security_groups = [aws_security_group.ecs_sg.id]
-    }
+  ingress {
+    protocol        = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
 
-    egress {
-        from_port       = 0
-        to_port         = 65535
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # iam for ecs
@@ -120,7 +123,7 @@ resource "aws_iam_role" "ecs_agent" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_agent" {
-  role       = "aws_iam_role.ecs_agent.name"
+  role       = aws_iam_role.ecs_agent.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
@@ -130,46 +133,55 @@ resource "aws_iam_instance_profile" "ecs_agent" {
 }
 
 resource "aws_launch_configuration" "ecs_launch_config" {
-    image_id             = "ami-094d4d00fd7462815"
-    iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
-    security_groups      = [aws_security_group.ecs_sg.id]
-    user_data            = "#!/bin/bash\necho ECS_CLUSTER=nz-gas-emissions-api >> /etc/ecs/ecs.config"
-    instance_type        = "t2.micro"
+  image_id             = "ami-0c02fb55956c7d316"
+  iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+  security_groups      = [aws_security_group.ecs_sg.id]
+  user_data            = "#!/bin/bash\necho ECS_CLUSTER=nz-gas-emissions-api >> /etc/ecs/ecs.config"
+  instance_type        = "t2.micro"
 }
 
 resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
-    name                      = "asg"
-    vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
-    launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+  name                 = "asg"
+  vpc_zone_identifier  = [aws_subnet.pub_subnet.id]
+  launch_configuration = aws_launch_configuration.ecs_launch_config.name
 
-    desired_capacity          = 2
-    min_size                  = 1
-    max_size                  = 3
-    health_check_grace_period = 300
-    health_check_type         = "EC2"
+  desired_capacity          = 2
+  min_size                  = 1
+  max_size                  = 3
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
 }
 
 # rds
-resource "aws_db_subnet_group" "db_subnet_group" {
-    subnet_ids  = [aws_subnet.pub_subnet.id]
-}
+# resource "aws_db_subnet_group" "db_subnet_group" {
+#  subnet_ids = [aws_subnet.pub_subnet.id]
+# }
 
 # ecs
-resource "aws_ecr_repository" "worker" {
-    name  = "worker"
-}
+# resource "aws_ecr_repository" "nz-gas-emissions-api" {
+#  name = "nz-gas-emissions-api"
+# }
 
 resource "aws_ecs_cluster" "ecs_cluster" {
-    name  = "nz-gas-emissions-api"
+  name = "nz-gas-emissions-api"
 }
 
 resource "aws_ecs_task_definition" "task_definition" {
-  family                = "worker"
-  container_definitions = data.template_file.task_definition_template.rendered
+  family = "nz-gas-emissions-api"
+  container_definitions = jsonencode([
+    {
+      "essential" : true,
+      "memory" : 512,
+      "name" : "nz-gas-emissions-api",
+      "cpu" : 2,
+      "image" : "454837591129.dkr.ecr.us-east-1.amazonaws.com/nz-gas-emissions-api:latest",
+      "environment" : []
+    }
+  ])
 }
 
-resource "aws_ecs_service" "worker" {
-  name            = "worker"
+resource "aws_ecs_service" "nz-gas-emissions-api" {
+  name            = "nz-gas-emissions-api"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = 2
